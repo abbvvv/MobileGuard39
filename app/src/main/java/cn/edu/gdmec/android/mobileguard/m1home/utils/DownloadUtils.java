1be4cbbc77d26950460bbc941e3de5e9dcb29098
许思航 <31603064+abbvvv@users.noreply.github.com>
2017-10-21 18:19:32 +0800

    。。。

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/DownloadUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/DownloadUtils.java
@@ -11,11 +11,11 @@ import android.webkit.MimeTypeMap;
  */
 
 public class DownloadUtils {
-    public void downapk(String url,String targetFile,Context context){
+    public void downloadApk(String url,String targetFile,Context context){
         DownloadManager.Request request=new DownloadManager.Request(Uri.parse(url));
         request.setAllowedOverRoaming(false);
         MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
-        String mimeString=mimeTypeMap.getExtensionFromMimeType(mimeTypeMap.getFileExtensionFromUrl(url));
+        String mimeString=mimeTypeMap.getMimeTypeFromExtension(mimeTypeMap.getFileExtensionFromUrl(url));
         request.setMimeType(mimeString);
 
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/VersionUpdateUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/VersionUpdateUtils.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.Message;
 import android.widget.Toast;
 
@@ -30,26 +31,25 @@ import cn.edu.gdmec.android.mobileguard.m1home.entity.VersionEntity;
 
 public class VersionUpdateUtils {
 
+    private String mVersion;
+    private Activity context;
+    private VersionEntity versionEntity;
 
-    private static final int MESSAGE_NET_ERROR=101;
+    //private static final int MESSAGE_NET_ERROR=101;
     private static final int MESSAGE_IO_ERROR=102;
     private static final int MESSAGE_JSON_ERROR=103;
     private static final int MESSAGE_SHOW_DIALOG=104;
     private static final int MESSAGE_ENTERHOME=105;
 
- private android.os.Handler handler=new android.os.Handler(){
+    private Handler handler=new Handler(){
      @Override
      public void handleMessage(Message msg) {
          switch (msg.what) {
              case MESSAGE_IO_ERROR:
-                 Toast.makeText(context, "IO错误", Toast.LENGTH_SHORT).show();
+                 Toast.makeText(context, "IO错误", Toast.LENGTH_LONG).show();
                  break;
              case MESSAGE_JSON_ERROR:
-                 Toast.makeText(context, "JSON解析错误", Toast.LENGTH_SHORT).show();
-                 break;
-             case MESSAGE_NET_ERROR:
-                 Toast.makeText(context,"网络异常",Toast.LENGTH_SHORT).show();
-                 enterHome();
+                 Toast.makeText(context, "JSON解析错误", Toast.LENGTH_LONG).show();
                  break;
              case MESSAGE_SHOW_DIALOG:
                  showUpdateDialog(versionEntity);
@@ -63,10 +63,8 @@ public class VersionUpdateUtils {
      }
  };
 
-    private String mVersion;
-    private Activity context;
-    private VersionEntity versionEntity;
-    private ProgressDialog mProgressDialog;
+
+    //private ProgressDialog mProgressDialog;
     public VersionUpdateUtils(String mVersion,Activity context) {
         this.mVersion = mVersion;
         this.context=context;
@@ -74,26 +72,23 @@ public class VersionUpdateUtils {
     public void getCloudVersion(){
 
         try {
-            HttpClient httpClient=new DefaultHttpClient();
-            HttpConnectionParams.setConnectionTimeout(httpClient.getParams(),5000);
-            HttpConnectionParams.setSoTimeout(httpClient.getParams(),5000);
+            HttpClient httpclient=new DefaultHttpClient();
+            HttpConnectionParams.setConnectionTimeout(httpclient.getParams(),5000);
+            HttpConnectionParams.setSoTimeout(httpclient.getParams(),5000);
             HttpGet httpGet=new HttpGet("http://android2017.duapp.com/updateinfo.html");
-            HttpResponse execute= httpClient.execute(httpGet);
+            HttpResponse execute= httpclient.execute(httpGet);
             if (execute.getStatusLine().getStatusCode()==200){
                 HttpEntity httpEntity=execute.getEntity();
                 String result= EntityUtils.toString(httpEntity,"utf-8");
                 JSONObject jsonObject=new JSONObject(result);
                 versionEntity=new VersionEntity();
-                String code=jsonObject.getString("code");
-                versionEntity.versionCode=code;
-                String des=jsonObject.getString("des");
-                versionEntity.description=des;
-                String apkurl=jsonObject.getString("apkurl");
-                versionEntity.apkurl=apkurl;
+                versionEntity.versionCode=jsonObject.getString("code");
+                versionEntity.description=jsonObject.getString("des");
+                versionEntity.apkurl=jsonObject.getString("apkurl");
                 if(!mVersion.equals(versionEntity.versionCode)){
-                    /*System.out.print(versionEntity.description);
-                    DownloadUtils downloadUtils=new DownloadUtils();
-                    downloadUtils.downloadApk(versionEntity.apkurl,"mobileguard.apk",context);*/
+                    //System.out.println(versionEntity.description);
+                   //DownloadUtils downloadUtils=new DownloadUtils();
+                    //downloadUtils.downloadApk(versionEntity.apkurl,"mobileguard.apk",context);
                     handler.sendEmptyMessage(MESSAGE_SHOW_DIALOG);
                 }
             }
@@ -112,7 +107,7 @@ public class VersionUpdateUtils {
         builder.setTitle("检查到有新版本"+versionEntity.versionCode);
         builder.setMessage(versionEntity.description);
         builder.setCancelable(false);
-        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setIcon(R.mipmap.ic_launcher_round);
         builder.setPositiveButton("立刻升级",new DialogInterface.OnClickListener(){
             @Override
             public void onClick(DialogInterface dialogInterface,int i){
@@ -129,10 +124,10 @@ public class VersionUpdateUtils {
         builder.show();
     }
     private void enterHome(){
-        handler.sendEmptyMessageDelayed(MESSAGE_ENTERHOME,2000);
+        handler.sendEmptyMessage(MESSAGE_ENTERHOME);
     }
     private void downloadNewApk(String apkurl){
         DownloadUtils downloadUtils=new DownloadUtils();
-        downloadUtils.downapk(apkurl,"/mnt/sdcard/mobilesafe2.0.apk",context);
+        downloadUtils.downloadApk(apkurl,"mobileguard.apk",context);
     }
 }
