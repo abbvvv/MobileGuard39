c2a5327c5c14ce80bbe8e63aef7d40f47f6c7acd
许思航 <31603064+abbvvv@users.noreply.github.com>
2017-12-07 17:48:50 +0800

    m9高级工具之号码归属地查询功能调试成功。

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -27,6 +27,7 @@ import cn.edu.gdmec.android.mobileguard.m4appmanager.AppManagerActivity;
 import cn.edu.gdmec.android.mobileguard.m5virusscan.VirusScanActivity;
 import cn.edu.gdmec.android.mobileguard.m6cleancache.CacheClearListActivity;
 import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.TrafficMonitoringActivity;
+import cn.edu.gdmec.android.mobileguard.m9advancedtools.AdvancedToolsActivity;
 
 /**
  * Created by student on 17/9/19.
@@ -74,6 +75,9 @@ public class HomeActivity extends AppCompatActivity{
                         break;
                     case 6:
                         startActivity(TrafficMonitoringActivity.class);
+                        break;
+                    case 7:
+                        startActivity(AdvancedToolsActivity.class);
                         break;
 
                 }
